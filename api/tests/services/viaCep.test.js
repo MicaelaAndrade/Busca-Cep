a16f6b336d9d596bd909347a0668e25b11cd0d36
MicaelaAndrade <micaela.andrade@outlook.com>
2020-09-02 11:59:21 -0300

    refatorando alguns dados da API

--- api/tests/services/viaCep.test.js
+++ api/tests/services/viaCep.test.js
@@ -3,7 +3,7 @@ const viaCep = require('../../services/viaCep')
 
 test('buscando cep', async () => {
     const cep = '21235280';
-
+    
     /** @type {Endereco} */
     const endereco = await viaCep(cep);
 
